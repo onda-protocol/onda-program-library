2c4052d2e8dc10d4af4f87591455d5c0f485c5ce
Grant Findlay <dev@grantfindlay.com>
2022-08-07 13:36:38 +0100

    update take_hire handler

--- programs/dexloan-listings/src/processor/hire/take.rs
+++ programs/dexloan-listings/src/processor/hire/take.rs
@@ -50,107 +50,107 @@ pub struct TakeHire <'info> {
 }
 
 pub fn handle_take_hire<'info>(ctx: Context<'_, '_, '_, 'info, TakeHire<'info>>, days: u16) -> Result<()> {
-  let hire = &mut ctx.accounts.hire_account;
-  let start_date = ctx.accounts.clock.unix_timestamp;
+    let hire = &mut ctx.accounts.hire_account;
+    let start_date = ctx.accounts.clock.unix_timestamp;
 
-  hire.state = HireState::Hired;
+    hire.state = HireState::Hired;
 
-  if hire.borrower.is_some() {
-      require_keys_eq!(hire.borrower.unwrap(), ctx.accounts.borrower.key());
-  } else {
-      hire.borrower = Some(ctx.accounts.borrower.key());
-  }
+    if hire.borrower.is_some() {
+        require_keys_eq!(hire.borrower.unwrap(), ctx.accounts.borrower.key());
+    } else {
+        hire.borrower = Some(ctx.accounts.borrower.key());
+    }
 
-  if hire.amount == 0 {
-      hire.current_expiry = Some(hire.expiry);
-  } else {
-      let amount = u64::from(days) * hire.amount;
-      let duration = i64::from(days) * SECONDS_PER_DAY;
-      let current_expiry = start_date + duration;
-      msg!("amount {}", amount);
-      msg!("duration {}", duration);
+    let duration = i64::from(days) * SECONDS_PER_DAY;
+    let current_expiry = start_date + duration;
 
-      if current_expiry > hire.expiry {
-          return err!(DexloanError::InvalidExpiry)
-      }
+    if current_expiry > hire.expiry {
+        return err!(DexloanError::InvalidExpiry)
+    }
 
-      hire.current_expiry = Some(current_expiry);
+    msg!("duration {}", duration);
+    hire.current_expiry = Some(current_expiry);
 
-      let remaining_amount = pay_creator_fees(
-          &mut ctx.remaining_accounts.iter(),
-          amount,
-          &ctx.accounts.mint.to_account_info(),
-          &ctx.accounts.metadata.to_account_info(),
-          &ctx.accounts.borrower.to_account_info(),
-      )?;
-  
-      // Transfer fee
-      anchor_lang::solana_program::program::invoke(
-          &anchor_lang::solana_program::system_instruction::transfer(
-              &hire.borrower.unwrap(),
-              &hire.lender,
-              remaining_amount,
-          ),
-          &[
-              ctx.accounts.borrower.to_account_info(),
-              ctx.accounts.lender.to_account_info(),
-          ]
-      )?;
-  }
 
-  // Thaw & Transfer NFT to hire account
-  let signer_bump = &[hire.bump];
-  let signer_seeds = &[&[
-      Hire::PREFIX,
-      hire.mint.as_ref(),
-      hire.lender.as_ref(),
-      signer_bump
-  ][..]];
+    if hire.amount > 0 {
+        let amount = u64::from(days) * hire.amount;
+        msg!("amount {}", amount);
 
-  thaw(
-      FreezeParams {
-          delegate: hire.to_account_info(),
-          token_account: ctx.accounts.deposit_token_account.to_account_info(),
-          edition: ctx.accounts.edition.to_account_info(),
-          mint: ctx.accounts.mint.to_account_info(),
-          signer_seeds,
-      }
-  )?;
-  anchor_spl::token::transfer(
-      CpiContext::new_with_signer(
-          ctx.accounts.token_program.to_account_info(),
-          anchor_spl::token::Transfer {
-              from: ctx.accounts.deposit_token_account.to_account_info(),
-              to: ctx.accounts.hire_token_account.to_account_info(),
-              authority: hire.to_account_info(),
-          },
-          signer_seeds
-      ),
-      1
-  )?;
+        let remaining_amount = pay_creator_fees(
+            &mut ctx.remaining_accounts.iter(),
+            amount,
+            &ctx.accounts.mint.to_account_info(),
+            &ctx.accounts.metadata.to_account_info(),
+            &ctx.accounts.borrower.to_account_info(),
+        )?;
+    
+        // Transfer fee
+        anchor_lang::solana_program::program::invoke(
+            &anchor_lang::solana_program::system_instruction::transfer(
+                &hire.borrower.unwrap(),
+                &hire.lender,
+                remaining_amount,
+            ),
+            &[
+                ctx.accounts.borrower.to_account_info(),
+                ctx.accounts.lender.to_account_info(),
+            ]
+        )?;
+    }
 
-  // Delegate authority & freeze hire token account
-  anchor_spl::token::approve(
-      CpiContext::new(
-          ctx.accounts.token_program.to_account_info(),
-          anchor_spl::token::Approve {
-              to: ctx.accounts.hire_token_account.to_account_info(),
-              delegate: hire.to_account_info(),
-              authority: ctx.accounts.borrower.to_account_info(),
-          }
-      ),
-      1
-  )?;
+    // Thaw & Transfer NFT to hire account
+    let signer_bump = &[hire.bump];
+    let signer_seeds = &[&[
+        Hire::PREFIX,
+        hire.mint.as_ref(),
+        hire.lender.as_ref(),
+        signer_bump
+    ][..]];
 
-  freeze(
-      FreezeParams {
-          delegate: hire.to_account_info(),
-          token_account: ctx.accounts.hire_token_account.to_account_info(),
-          edition: ctx.accounts.edition.to_account_info(),
-          mint: ctx.accounts.mint.to_account_info(),
-          signer_seeds: signer_seeds
-      }
-  )?;
+    thaw(
+        FreezeParams {
+            delegate: hire.to_account_info(),
+            token_account: ctx.accounts.deposit_token_account.to_account_info(),
+            edition: ctx.accounts.edition.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            signer_seeds,
+        }
+    )?;
+    anchor_spl::token::transfer(
+        CpiContext::new_with_signer(
+            ctx.accounts.token_program.to_account_info(),
+            anchor_spl::token::Transfer {
+                from: ctx.accounts.deposit_token_account.to_account_info(),
+                to: ctx.accounts.hire_token_account.to_account_info(),
+                authority: hire.to_account_info(),
+            },
+            signer_seeds
+        ),
+        1
+    )?;
 
-  Ok(())
+    // Delegate authority & freeze hire token account
+    anchor_spl::token::approve(
+        CpiContext::new(
+            ctx.accounts.token_program.to_account_info(),
+            anchor_spl::token::Approve {
+                to: ctx.accounts.hire_token_account.to_account_info(),
+                delegate: hire.to_account_info(),
+                authority: ctx.accounts.borrower.to_account_info(),
+            }
+        ),
+        1
+    )?;
+
+    freeze(
+        FreezeParams {
+            delegate: hire.to_account_info(),
+            token_account: ctx.accounts.hire_token_account.to_account_info(),
+            edition: ctx.accounts.edition.to_account_info(),
+            mint: ctx.accounts.mint.to_account_info(),
+            signer_seeds: signer_seeds
+        }
+    )?;
+
+    Ok(())
 }
