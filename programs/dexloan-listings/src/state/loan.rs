c0c2a4207f591b6e0a243b04bf14566c639d3f70
Grant Findlay <dev@grantfindlay.com>
2022-09-07 08:46:36 +0100

    update loan state

--- programs/dexloan-listings/src/state/loan.rs
+++ programs/dexloan-listings/src/state/loan.rs
@@ -2,6 +2,7 @@ use anchor_lang::prelude::*;
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, PartialEq)]
 pub enum LoanState {
+    Unlisted,
     Listed,
     Active,
     Defaulted,
@@ -11,22 +12,28 @@ pub enum LoanState {
 pub struct Loan {
     /// Whether the loan is active
     pub state: LoanState,
+    /// Loan to floor value
+    pub ltv: Option<u32>,
     /// The amount of the loan
-    pub amount: u64,
+    pub amount: Option<u64>,
+    /// The liquidation threshold
+    pub threshold: Option<u32>,
+    /// The secondary sale price
+    pub sale_price: Option<u64>,
     /// The NFT holder
     pub borrower: Pubkey,
     /// The issuer of the loan
-    pub lender: Pubkey,
+    pub lender: Option<Pubkey>,
     /// Annualized return
     pub basis_points: u32,
     /// Duration of the loan in seconds
     pub duration: i64,
     /// The start date of the loan
-    pub start_date: i64,
+    pub start_date: Option<i64>,
     /// The mint of the token being used for collateral
     pub mint: Pubkey,
     /// Misc
-    pub padding: [u8; 64],
+    pub padding: [u8; 32],
     pub bump: u8,
 }
 
@@ -34,14 +41,17 @@ impl Loan {
     pub fn space() -> usize {
         8 + // key
         1 + // state
-        8 + // amount
+        (1 + 4) + // Loan to value
+        (1 + 8) + // amount
+        (1 + 4) + // threshold
+        (1 + 8) + // Sale price
         32 + // borrower
-        32 + // lender
+        (1 + 32) + // lender
         4 + // basis_points
         8 + // duration
-        8 + // start_date
+        (1 + 8) + // start_date
         32 + // mint
-        64 + // padding
+        32 + // padding
         1 // bump
     }
 
